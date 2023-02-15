12d11dfecb850746a4b3eec3b70ccc17c6d501fd
Tobyisepik <116378333+Tobyisepik@users.noreply.github.com>
2023-02-15 14:59:59 +0000

    double

---
+++ Website Search/script.js
@@ -0,0 +1,34 @@
+const userCardTemplate=document.querySelector("[data-user-template]");
+const userCardContainer=document.querySelector("[data-user-cards-container]");
+const searchInput=document.querySelector("[data-search]");
+
+let users=[];
+
+searchInput.addEventListener("input",(e)=>{
+    const value = e.target.value;
+    users.forEach(user=>{
+        const isVisible = 
+        user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value) || //lower case check letters
+        user.name.toUpperCase().includes(value) || user.email.toUpperCase().includes(value);   // upper case check letters
+        
+
+        user.element.classList.toggle("hide",!isVisible)
+    })
+    console.log(users);
+})
+
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+        users =data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0];
+            const header = card.querySelector("[data-header]");
+            const body =card.querySelector("[data-body]");
+            header.textContent=user.name;
+            body.textContent=user.email;
+            userCardContainer.append(card);
+            return { name: user.name, email: user.email, element: card};
+        })
+    })
