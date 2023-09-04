346c9d99bd60a5f5e2432351dfa4506f99f8a68c
Tobyisepik <116378333+Tobyisepik@users.noreply.github.com>
2023-09-04 21:45:38 +0100

    a

--- ProjectIntro.js
+++ ProjectIntro.js
@@ -1,7 +1,7 @@
 
 setInterval(() => {
   if (document.title==='Projects'){
-    document.title='Projects - By Toby';
+    document.title='Projects - By Toby Stanislaus';
   }else{
     document.title='Projects';
   }
